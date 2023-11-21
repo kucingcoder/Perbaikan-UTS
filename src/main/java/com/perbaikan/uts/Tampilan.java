067f9af852f99a4aec19a569c44ee39fb766337a
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2023-11-21 11:54:03 +0700

    Hapus Isi dulu

--- src/main/java/com/perbaikan/uts/Tampilan.java
+++ src/main/java/com/perbaikan/uts/Tampilan.java
@@ -169,6 +169,10 @@ public class Tampilan extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        nim.setText("");
+        nama.setText("");
+        email.setText("");
+        
         if (input.getText().isEmpty()) {
             JOptionPane.showMessageDialog(this, "Input NIM masih kosong\nharap diisi");
         } else {
